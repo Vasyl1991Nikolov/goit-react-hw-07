22167885d7ae372c46d1f3ac4c8446c8ba0e3d96
Vasyl Nikolov <vnnikolov1991@gmail.com>
2024-12-09 14:39:34 +0100

    fixet slice

--- src/api/contactsApi.js
+++ src/api/contactsApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://6754c03736bcd1eec851e88a.mockapi.io/'; // Замените на ваш URL
+const API_URL = 'https://6754c03736bcd1eec851e88a.mockapi.io/'; 
 
 export const fetchContactsFromAPI = async () => {
   const response = await axios.get(API_URL);
